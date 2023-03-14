a21163edaf44ca9bcf9181cc2eb01451319aabae
Salim <salim.belkhir@etu.umontpellier.fr>
2023-03-14 09:56:54 +0100

    Initial Commit

---
+++ Befest_mobile/Befest_mobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  Befest_mobileApp.swift
+//  Befest_mobile
+//
+//  Created by etud on 14/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Befest_mobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
