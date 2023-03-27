ba41799a0762ff9f669ae486f7f7239b771b85eb
Salim-belkhir <salim.belkhir@etu.umontpellier.fr>
2023-03-27 10:07:05 +0200

    Changes

---
+++ Befest_mobile/views/General/Affectation/ListAffectationView.swift
@@ -0,0 +1,20 @@
+//
+//  ListAffectationView.swift
+//  Befest_mobile
+//
+//  Created by etud on 27/03/2023.
+//
+
+import SwiftUI
+
+struct ListAffectationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ListAffectationView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListAffectationView()
+    }
+}
