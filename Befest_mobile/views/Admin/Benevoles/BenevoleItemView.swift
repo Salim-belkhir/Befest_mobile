706c3335d089b7276448ce8fb84d5e8b0acaf13d
Salim-belkhir <salim.belkhir@etu.umontpellier.fr>
2023-03-27 10:02:29 +0200

    A lot of changes

---
+++ Befest_mobile/views/Admin/Benevoles/BenevoleItemView.swift
@@ -0,0 +1,20 @@
+//
+//  BenevoleItemView.swift
+//  Befest_mobile
+//
+//  Created by etud on 27/03/2023.
+//
+
+import SwiftUI
+
+struct BenevoleItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BenevoleItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        BenevoleItemView()
+    }
+}
---
+++ Befest_mobile/views/Benevole/Dispos/DispoItemView.swift
@@ -0,0 +1,20 @@
+//
+//  DispoItemView.swift
+//  Befest_mobile
+//
+//  Created by etud on 27/03/2023.
+//
+
+import SwiftUI
+
+struct DispoItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DispoItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        DispoItemView()
+    }
+}
